400b5409a525689a8138d99d3faff57cf7d675de
Carlos Roldán Piñero <101227114+TxarliRoldn@users.noreply.github.com>
2022-12-01 00:10:47 +0100

    merged with raw.jl and moved to json

--- obs.jl
+++ obs.jl
@@ -1,8 +1,7 @@
 using ADerrors
-using CSV
-using DataFrames
-using DataStructures
 using JLD2
+using JSON
+using LinearAlgebra
 using SpecialFunctions
 using Statistics
 using TOML
@@ -10,149 +9,225 @@ using TOML
 wpm    = Dict{String,Vector{Float64}}()
 chosen = [-1, 1.5, -1, -1]
 
-function alphaMS(r0, powa=3, Nf=0)
-    lambdaMS = uwreal([0.660, 0.011], "reference_lambda_MS")
-    logmuLam = log(r0^2/lambdaMS^2)
+###########################
+### PARSE THE TOML FILE ###
+###########################
 
-    beta0 = ( 33.0 - 2.0 * Nf )/( 12.0 * pi )
-    beta1 = ( 153.0 - 19.0 * Nf )/( 24.0 * pi^2 )
-    beta2 = ( 77139.0 - 15099.0 * Nf + 325.0 * Nf^2 )/( 3456.0 * pi^3 )
-    beta3 = 1/( 4.0 * pi )^4*( 149753.0/6.0 + 3564.0 * zeta(3) - ( 1078361.0 / 162.0 + 6508.0 / 27.0 * zeta(3) ) * Nf  + ( 50065.0 / 162.0 + 6472.0 / 81.0 * zeta(3) ) * Nf^2 + 1093.0 / 729.0 * Nf^3 )   
+function inParse(filename)
+    params = TOML.parsefile(filename)
 
-    a_N0LO =  1.0 / (beta0 * logmuLam)
+    # Global field
+    id  = params["id"]
+    fls = params["files"]
+    uis = params["uids"]
 
-    a_N1LO = -beta1 * log(logmuLam) / ( beta0^3 * (logmuLam)^2 )
+    # Simulation info
+    bts = params["Simulation"]["betas"]
+    flw = params["Simulation"]["flow"]
 
-    a_N2LO = 1.0 / ( beta0^3 * (logmuLam)^3 ) * ( beta1^2 / beta0^2 * ( ( log(logmuLam) )^2 - log(logmuLam) - 1.0 ) + beta2 / beta0 )
-    
-    a_N3LO = 1.0 / ( beta0^4 * (logmuLam)^4 ) * ( beta1^3/beta0^3 * ( -( log(logmuLam) )^3 + 5.0/2.0 * ( log(logmuLam) )^2 + 2.0 * log(logmuLam) -1.0/2.0 ) -3.0 * beta1*beta2/beta0^2* log(logmuLam) + beta3 / ( 2.0 * beta0 ) )
+    # Analysis peculiarities
+    xi  = params["Analysis"]["xi"]
+    rgB = params["Analysis"]["rg"]
 
-    alpha_ms = a_N0LO*(powa >= 0) + a_N1LO*(powa >= 1) + a_N2LO*(powa >= 2) + a_N3LO*(powa >= 3)
-    return alpha_ms
+    # Action density
+    En = params["Action_density"]["names"]
+    Ev = params["Action_density"]["values"]
+
+    # Derivative of action density
+    dEn = params["Derivative_action_density"]["names"]
+    dEv = params["Derivative_action_density"]["values"]
+
+    return id, fls, uis, bts, flw, xi, rgB, En, Ev, dEn, dEv
 end
 
-dchi(r0, t) = 27/56 * ((alphaMS(r0)/alphaMS(r0/sqrt(8*t)))^(7/11) - 1.0)
+#############################################
+### FUNCTIONS TO INTERACT WITH JLD2 FILES ###
+#############################################
 
-function getObs(y, x, refVal, mapfun=nothing)
-    r   = [findall(y .< refVal)[end], findall(y .> refVal)[1]]
-    obs = (x[r[1]]*(y[r[2]]-refVal) - x[r[2]]*(y[r[1]]-refVal))/(y[r[2]] - y[r[1]])
-    if mapfun !== nothing
-        obs = mapfun(obs)
+function derivate(y, x)
+    dy = Array{Float64}(undef, size(y)[1], size(y)[2]-2)
+    for i in 2:(length(x)-1)
+        dy[:,i-1] = (y[:,i+1] .- y[:,i-1]) ./ (x[i+1] - x[i-1])
     end
+    return dy
+end
+
+function loadXsl(file, discretization) # discretization = Ysl or Wsl
+	data  = jldopen(file, "r")
+	nmeas = parse(Int64, keys(data[discretization])[end])
+	sfq   = parse(Int64, keys(data[discretization])[1])
+	nfw   = size(data["tfw"])[1]
+	tfw   = data["tfw"][:]
+
+	Xsl = Array{Float64}(undef, nmeas, nfw)
+	for (i,key) in enumerate(keys(data[discretization]))
+		Xsl[(i-1)*sfq+1:i*sfq, :] = mean(data[discretization][key][:,:,:,:], dims=3)[:,:,1,1]
+	end
+	close(data)
+
+    ttX    = tfw'.^2 .* Xsl
+    tdttX  = tfw[2:end-1]' .* derivate(ttX, tfw)
+    ttX    = ttX[:,2:end-1]
+	tfw    = tfw[2:end-1]
+
+	return tfw, ttX, tdttX
+end
+
+###########################################
+### FUNCTIONS FOR RENORMALIZATION GROUP ###
+###########################################
+
+function r0aPoly(beta)
+    global wpm["r0a.$beta"] = chosen
+    if (beta < 5.7) || (beta > 6.9)
+        exit()
+    elseif beta < 6
+        r0a  = exp(1.6805 + 1.7139*(beta - 6) - 0.8155*(beta - 6)^2 + 0.6667*(beta - 6)^3)
+        dr0a = (0.003/0.87 * (beta - 5.7) + 0.003) * r0a
+        ur0a = uwreal([r0a, dr0a], "r0a.$beta")
+        uwerr(ur0a)
+        return ur0a
+    else
+        r0a  = 9.5130 + 12.965*(beta - 6.38) + 6.982*(beta - 6.38)^2 + 4.16*(beta - 6.38)^3
+        dr0a = (0.01 * (beta - 6) + 0.001) * r0a
+        ur0a = uwreal([r0a, dr0a], "r0a.$beta")
+        uwerr(ur0a)
+        return ur0a
+    end
+end
+
+function alphaMS(beta, mulr0=1, powa=3, Nf=0)
+    r0 = r0aPoly(beta) * mulr0
+    lambdaMS  = uwreal([0.660, 0.011], "refLamMS")
+    logmuLam  = log(r0^2/lambdaMS^2)
+    llogmuLam = log(logmuLam)
+
+    zeta3 = zeta(3)
+    beta0 = (33.0 - 2.0*Nf)/(12.0*pi)
+    beta1 = (153.0 - 19.0*Nf)/(24.0*pi^2)
+    beta2 = (77139.0 - 15099.0*Nf + 325.0*Nf^2)/(3456.0*pi^3)
+    beta3 = (149753.0/6.0 + 3564.0*zeta3 - (1078361.0/162.0 + 6508.0/27.0*zeta3)*Nf + (50065.0/162.0 + 6472.0/81.0*zeta3)*Nf^2 + 1093.0/729.0*Nf^3)/(4.0*pi)^4
+
+    aN0LO = 1.0/(beta0*logmuLam)
+    aN1LO = -beta1*llogmuLam/(beta0^3*logmuLam^2)
+    aN2LO = 1.0/(beta0^3*logmuLam^3)*(beta1^2/beta0^2*(llogmuLam^2 - llogmuLam - 1.0) + beta2/beta0)
+    aN3LO = 1.0/(beta0^4*logmuLam^4)*(beta1^3/beta0^3*(-llogmuLam^3 + 5.0/2.0*llogmuLam^2 + 2.0*llogmuLam -1.0/2.0) - 3.0*beta1*beta2/beta0^2*llogmuLam + beta3/(2.0*beta0))
+
+    alphaMS = aN0LO*(powa >= 0) + aN1LO*(powa >= 1) + aN2LO*(powa >= 2) + aN3LO*(powa >= 3)
+    return alphaMS
+end
+
+dxi(beta, t) = 27/56 * ((alphaMS(beta)/alphaMS(beta, 1/sqrt(8*t)))^(7/11) - 1.0)
+
+###############################################
+### GET THE VALUES NEEDED FOR EXTRAPOLATION ###
+###############################################
+
+function fileIteration(fl, bt, xi, rgB, En, Ev, dEn, dEv)
+    nE, ndE  = length(Ev), length(dEv)
+
+    # Load the JLD2 files
+    t, Y, dY = loadXsl(fl, "Ysl")
+    t, W, dW = loadXsl(fl, "Wsl")
+    nMs, nFw = size(Y)
+
+    # Preallocate arrays
+    rt  = Array{Float64}(undef, 2*nE)
+    drt = Array{Float64}(undef, 2*ndE)
+    rE  = Array{uwreal}(undef,  2*nE)
+    drE = Array{uwreal}(undef,  2*ndE)
+
+    # Fast iteration for the flow times
+    Et0, dEt0, Ec, dEc = 0.0, 0.0, 1, 1
+    for i in 1:nFw
+        tX  = xi + rgB * dxi(bt, t[i]) # Adjusted xi for RG
+        otX = 1.0 - tX
+
+        Et1  = tX*mean(W[:,i])  + otX*mean(Y[:,i])
+        dEt1 = tX*mean(dW[:,i]) + otX*mean(dY[:,i])
+
+        if Ec != nE + 1
+            if (Et0 < Ev[Ec]) && (Et1 > Ev[Ec])
+                rE[2*Ec]   = tX*uwreal(W[:,i], "pl.$fl")   + otX*uwreal(Y[:,i], "cl.$fl")
+                rE[2*Ec-1] = tX*uwreal(W[:,i-1], "pl.$fl") + otX*uwreal(Y[:,i-1], "cl.$fl")
+                rt[2*Ec]   = t[i]
+                rt[2*Ec-1] = t[i-1]
+                Ec += 1
+            end
+        end
+        if dEc != ndE + 1
+            if (dEt0 < dEv[dEc]) && (dEt1 > dEv[dEc])
+                drE[2*dEc]   = tX*uwreal(dW[:,i], "pl.$fl")   + otX*uwreal(dY[:,i], "cl.$fl")
+                drE[2*dEc-1] = tX*uwreal(dW[:,i-1], "pl.$fl") + otX*uwreal(dY[:,i-1], "cl.$fl")
+                drt[2*dEc]   = t[i]
+                drt[2*dEc-1] = t[i-1]
+                dEc += 1
+            end
+        end
+        Et0, dEt0 = Et1, dEt1
+    end
+
+    return rt, drt, rE, drE
+end
+
+#########################################
+### FUNCTIONS TO BUILD THE DICTIONARY ###
+#########################################
+
+function getObs!(dict, y, x, refVal, name)
+    r1, r2 = findall(y .< refVal)[end], findall(y .> refVal)[1]
+    obs    = x[r1] + (refVal - y[r1]) * (y[r2] - y[r1])/(x[r2] - x[r1])
     uwerr(obs, wpm)
-    return value(obs), err(obs)
+    dict[name] = Dict("ADerr"        => obs,
+                      "Value"        => value(obs),
+                      "Uncertainty"  => err(obs),
+                      "Correlations" => Dict())
+end
+
+function corrDict!(dict)
+    for n1 in keys(dict)
+        for n2 in keys(dict)
+            if n1 == n2
+                continue
+            end
+            tempMat = ADerrors.cov([dict[n1]["ADerr"], dict[n2]["ADerr"]], wpm)
+            dict[n1]["Correlations"][n2] = tempMat[1,2]/sqrt(tempMat[1,1]*tempMat[2,2])
+        end
+    end
+end
+
+function cleanDict!(dict)
+    for name in keys(dict)
+        delete!(dict[name], "ADerr")
+    end
 end
 
 function main()
-    df = DataFrame()
-    params = TOML.parsefile(ARGS[1])
+    inFile  = ARGS[1] # to be changed
+    outFile = ARGS[2]
+    id, fls, uis, bts, flw, xi, rgB, En, Ev, dEn, dEv = inParse(inFile)
 
-    volumes  = params["General"]["volumes"]
-    prefixes = params["General"]["flows"]
-    chis     = params["General"]["coefs"]
-    r0s      = Array{uwreal}(undef, length(volumes))
-    if params["General"]["RG"]
-        for (i, volume) in enumerate(volumes)
-            global wpm["r0a"*volume] = chosen
-            r0s[i] = uwreal([params["General"]["sommer_scales"][i], params["General"]["err_sommer_scales"][i]], "r0a"*volume)
-            uwerr(r0s[i], wpm)
-        end
-    end
+    dictObs = Dict(id => [])
 
-    ttENames  = params["Action_density"]["names"]
-    ttEValues = params["Action_density"]["values"]
-    ttEMaps   = params["Action_density"]["functions"]
-
-    tdttENames  = params["Derivative_action_density"]["names"]
-    tdttEValues = params["Derivative_action_density"]["values"]
-    tdttEMaps   = params["Derivative_action_density"]["functions"]
-
-    for (prefix, chi) in zip(prefixes, chis)
-        for (j, volume) in enumerate(volumes)
-            tchi = 0.0
-
-            observables = OrderedDict()
-            observables["id"]     = params["General"]["id"]
-            observables["flow"]   = prefix
-            observables["volume"] = volume
+    for (i, (fl, bt, ui)) in enumerate(zip(fls, bts, uis))
+        append!(dictObs[id], [Dict("UID" => ui, "Beta" => bt, "Flow" => flw, "Observables" => Dict())])
             
-            global wpm["plaquette"*prefix*volume] = chosen
-            global wpm["clover"*prefix*volume]    = chosen
-
-            fileY = jldopen("JLD2_DIR/"*prefix*"Y"*volume*".jld2", "r")
-            fileW = jldopen("JLD2_DIR/"*prefix*"W"*volume*".jld2", "r")
-            nmeas = size(fileY["ttE"])[1]
-            nflow = size(fileY["ttE"])[2]
-            tfw   = Array{Float64}(undef, 2*length(ttEValues))
-            dtfw  = Array{Float64}(undef, 2*length(tdttEValues))
-            tte   = Array{uwreal}(undef, 2*length(ttEValues))
-            tdtte = Array{uwreal}(undef, 2*length(tdttEValues))
-
-            ttEtemp0   = 0
-            tdttEtemp0 = 0
-            ttEcont    = 1
-            tdttEcont  = 1
-            for i in 1:nflow
-                if params["General"]["RG"]
-                    tchi = chi + dchi(r0s[j], fileY["tfw"][i])
-                else
-                    tchi = chi
-                end
-
-                ttEtemp1   = tchi*mean(fileW["ttE"][:,i]) + (1-tchi)*mean(fileY["ttE"][:,i])
-                tdttEtemp1 = tchi*mean(fileW["tdttE"][:,i]) + (1-tchi)*mean(fileY["tdttE"][:,i])
-
-                if ttEcont != length(ttEValues)+1
-                    if (ttEtemp0 < ttEValues[ttEcont]) && (ttEtemp1 > ttEValues[ttEcont])
-                        tte[2*ttEcont]   = tchi*uwreal(fileW["ttE"][:,i], "plaquette"*prefix*volume) + (1-tchi)*uwreal(fileY["ttE"][:,i], "clover"*prefix*volume)
-                        tte[2*ttEcont-1] = tchi*uwreal(fileW["ttE"][:,i-1], "plaquette"*prefix*volume) + (1-tchi)*uwreal(fileY["ttE"][:,i-1], "clover"*prefix*volume)
-                        tfw[2*ttEcont]   = fileY["tfw"][i]
-                        tfw[2*ttEcont-1] = fileY["tfw"][i-1]
-                        ttEcont += 1
-                    end
-                end
-                if tdttEcont != length(tdttEValues)+1
-                    if (tdttEtemp0 < tdttEValues[tdttEcont]) && (tdttEtemp1 > tdttEValues[tdttEcont])
-                        tdtte[2*tdttEcont]   = tchi*uwreal(fileW["tdttE"][:,i], "plaquette"*prefix*volume) + (1-tchi)*uwreal(fileY["tdttE"][:,i], "clover"*prefix*volume)
-                        tdtte[2*tdttEcont-1] = tchi*uwreal(fileW["tdttE"][:,i-1], "plaquette"*prefix*volume) + (1-tchi)*uwreal(fileY["tdttE"][:,i-1], "clover"*prefix*volume)
-                        dtfw[2*tdttEcont]    = fileY["tfw"][i]
-                        dtfw[2*tdttEcont-1]  = fileY["tfw"][i-1]
-                        tdttEcont += 1
-                    end
-                end
-
-                ttEtemp0   = ttEtemp1
-                tdttEtemp0 = tdttEtemp1
-            end
-            close(fileY)
-            close(fileW)
-
-            for (name, refVal, mapfun) in zip(ttENames, ttEValues, ttEMaps)
-                if mapfun != 0
-                    observables[name], observables["err_"*name] = getObs(tte, tfw, refVal, getfield(Main, Symbol(mapfun)))
-                else
-                    observables[name], observables["err_"*name] = getObs(tte, tfw, refVal)
-                end
-            end
-            for (name, refVal, mapfun) in zip(tdttENames, tdttEValues, tdttEMaps)
-                if mapfun != 0
-                    observables[name], observables["err_"*name] = getObs(tdtte, tfw, refVal, getfield(Main, Symbol(mapfun)))
-                else
-                    observables[name], observables["err_"*name] = getObs(tdtte, tfw, refVal)
-                end
-            end
-
-            if !params["General"]["append"] && (prefix == prefixes[1]) && (volume == volumes[1])
-                df = DataFrame(observables)
-            elseif params["General"]["append"] && (prefix == prefixes[1]) && (volume == volumes[1])
-                df = CSV.read(ARGS[2], DataFrame)
-                push!(df, observables)
-            else
-                push!(df, observables)
-            end
+        rt, drt, rE, drE = fileIteration(fl, bt, xi, rgB, En, Ev, dEn, dEv)
+        
+        for (name, refVal) in zip(En, Ev)
+            getObs!(dictObs[id][end]["Observables"], rE,  rt,  refVal, name)
         end
+        for (name, refVal) in zip(dEn, dEv)
+            getObs!(dictObs[id][end]["Observables"], drE, drt, refVal, name)
+        end
+        corrDict!(dictObs[id][end]["Observables"])
+        cleanDict!(dictObs[id][end]["Observables"])
     end
-    CSV.write(ARGS[2], df)
+
+    dictSave = isfile(outFile) ? merge(JSON.parsefile(outFile), dictObs) : dictObs
+    open(outFile, "w") do io
+        JSON.print(io, dictSave)
+    end
 end
 
 main()
